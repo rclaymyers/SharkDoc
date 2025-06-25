cca6d581d2ea60bf7ebfd51d02989bb6da5b2983
Clay Myers <myers_richard1@protonmail.com>
2025-06-25 12:01:07 -0500

    5 Integrate gallery

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,11 +2,9 @@
 import { ref } from "vue";
 import MarkdownDisplay from "./MarkdownDisplay.vue";
 import MarkdownEditor from "./MarkdownEditor.vue";
+import ImageGallery from "./ImageGallery.vue";
 
-defineProps<{ msg: string }>();
-
-let markdownText = ref("# test\n## test 2\n*italics*\ngallery(testGallery)");
-
+const markdownText = ref("# test\n## test 2\n*italics*\ngallery(testGallery)");
 const updateMarkdownText = (newValue: string) => {
   markdownText.value = newValue;
 };
@@ -25,24 +23,7 @@ const toggleEditor = () => {
     @update:markdownText="updateMarkdownText"
     v-if="showEditor"
   ></MarkdownEditor>
-
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <ImageGallery></ImageGallery>
 </template>
 
 <style scoped>
---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+import FsLightbox from "fslightbox-vue";
+
+const toggler = ref(false);
+</script>
+
+<template>
+  <button @click="toggler = !toggler">Open the lightbox.</button>
+  <FsLightbox
+    :toggler="toggler"
+    :sources="[
+      'https://i.imgur.com/fsyrScY.jpg',
+      'https://www.youtube.com/watch?v=3nQNiWdeH2Q',
+      'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+    ]"
+  />
+</template>
