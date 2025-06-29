d0a0e06d3258b536ad430ed9d449fd3374a7373f
rclaymyers <myers_richard1@protonmail.com>
2025-06-29 16:18:42 -0500

    39 fix gallery creation (#42)
    * 39 WIP
    
    * 39 Fix gallery creation form

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,9 +11,11 @@ import Aura from "@primeuix/themes/aura";
 //import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
+import { Dialog } from "primevue";
 
 const app = createApp(App);
 app.use(router);
 app.use(PrimeVue, { theme: { preset: Aura } });
 app.component("Galleria", Galleria);
+app.component("Dialog", Dialog);
 app.mount("#app");
