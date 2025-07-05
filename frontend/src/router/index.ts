fef8df036abbf30f26fa28c1f8e3b11f75ba664b
Clay Myers <myers_richard1@protonmail.com>
2025-07-05 18:05:31 -0500

    70 Redirect to signin page if login isnt valid

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import DocumentList from "../components/DocumentList.vue";
 import DocumentViewer from "../components/DocumentViewer.vue";
 import UserLoginRegistration from "../components/UserLoginRegistration.vue";
+import { LocalStorageService } from "../services/localStorageService";
 
 const routes = [
   { path: "/", name: "Home", component: DocumentList },
@@ -14,5 +15,28 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+const publicPaths = ["/signin"];
+router.beforeEach((to, from, next) => {
+  const token = LocalStorageService.getJwt();
+  if (!tokenValid(token) && !publicPaths.includes(to.path)) {
+    next("/signin");
+  } else {
+    next();
+  }
+});
+
+const tokenValid = (token: string | null): boolean => {
+  if (!token) {
+    return false;
+  }
+  try {
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    const currentTime = Math.floor(Date.now() / 1000);
+    return !payload?.exp || payload.exp >= currentTime;
+  } catch (e) {
+    return false;
+  }
+};
 
 export default router;
