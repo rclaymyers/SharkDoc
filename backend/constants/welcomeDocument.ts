0c4aa99cd119db39e0515bc4c65d647b2d0e6e6b
rclaymyers <myers_richard1@protonmail.com>
2025-07-06 13:15:19 -0500

    94 Create welcome document for new users (#108)

--- backend/constants/welcomeDocument.ts
+++ backend/constants/welcomeDocument.ts
@@ -2,7 +2,7 @@ export const WelcomeDocumentPage1 = `
 # Welcome to Sharkdown!
 This is a markdown document. Try clicking the pencil icon above to open the editor.
 
-Unlike most markdown editors, SharkDoc supports documents with multiple pages and image galleries.
+Unlike most markdown editors, Sharkdown supports documents with multiple pages and image galleries.
 `;
 
 export const WelcomeDocumentPage2 = `
@@ -12,6 +12,6 @@ Click the + icon below this page to add a new page.
 You can add galleries by clicking the "Manage Galleries" button above. 
 On mobile, the button is in the hamburger menu at the top right.
 
-After adding a gallery, you can insert it into the document using the following syntax:
-gallery\ (galleryName)
+After adding a gallery, you can insert it into the document like so:
+gallery(dogsAndCats)
 `;
